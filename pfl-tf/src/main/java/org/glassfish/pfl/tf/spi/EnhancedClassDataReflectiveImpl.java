9dbfc95456fd7b3680c9da810eb5fb5d2e2156dd
Jonathan Coustick <jonathan.coustick@payara.fish>
2020-03-19 15:58:41 +0000

    Convert pfl-tf to use asm from upstream
    Signed-off-by: Jonathan Coustick <jonathan.coustick@payara.fish>

--- pfl-tf/src/main/java/org/glassfish/pfl/tf/spi/EnhancedClassDataReflectiveImpl.java
+++ pfl-tf/src/main/java/org/glassfish/pfl/tf/spi/EnhancedClassDataReflectiveImpl.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020 Payara Services Ltd.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -21,7 +22,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import org.glassfish.pfl.objectweb.asm.Type;
+import org.objectweb.asm.Type;
 
 public class EnhancedClassDataReflectiveImpl extends EnhancedClassDataBase {
     private final Class<?> currentClass ;
